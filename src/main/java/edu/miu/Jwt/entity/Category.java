55c0480340959c8fccb2c68fa1becf2917286a14
Meresa <meresa27@gmail.com>
2022-05-05 14:38:05 -0500

    entities created

---
+++ src/main/java/edu/miu/Jwt/entity/Category.java
@@ -0,0 +1,25 @@
+package edu.miu.Jwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @JsonBackReference
+    @ManyToMany
+    private List<Product> products;
+}
---
+++ src/main/java/edu/miu/Jwt/entity/Review.java
@@ -0,0 +1,18 @@
+package edu.miu.Jwt.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String comment;
+    private int numberOfStars;
+
+    @ManyToOne
+    private Product product;
+}
